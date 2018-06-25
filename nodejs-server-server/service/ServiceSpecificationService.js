0157caaad486643dce6769ef18b301de3c34c8fa
Thomas <lester.thomas@vodafone.com>
2018-06-25 17:46:17 +0100

    Fixed all eslint problems

--- nodejs-server-server/service/ServiceSpecificationService.js
+++ nodejs-server-server/service/ServiceSpecificationService.js
@@ -7,55 +7,54 @@ var yaml = require('js-yaml');
 exports.getServiceSpecifications = function() {
   return new Promise(function(resolve, reject) {
 
-    var hostName = "http://localhost:8080";
-    var basePathName = "/tmf-api/serviceCatalogManagement/v2/";
+    var hostName = 'http://localhost:8080';
+    var basePathName = '/tmf-api/serviceCatalogManagement/v2/';
     var fileArray = [];
     var filestarttemplate = 'service-';
     var fileendtemplate = '-template.yml';
     fs.readdirSync('TOSCA').forEach((file) => {
-      if ((file.substring(file.length-fileendtemplate.length) == fileendtemplate) && (file.substring(0,filestarttemplate.length) == filestarttemplate)) {
+      if ((file.substring(file.length - fileendtemplate.length) === fileendtemplate) && (file.substring(0, filestarttemplate.length) === filestarttemplate)) {
         fileArray.push(file);
       }
     });
 
     var outputServiceSpecArray = [];
-    //for (var file_index=0;file_index<fileArray.length;file_index++){
+    // for (var file_index=0;file_index<fileArray.length;file_index++){
     fileArray.forEach(function(fileName){
-      var serviceName = fileName.substring(filestarttemplate.length, fileName.length-fileendtemplate.length);
-      console.log('Service Name',serviceName);
-      fs.readFile('TOSCA/'+fileName, 'utf8', function (err, data) {
+      var serviceName = fileName.substring(filestarttemplate.length, fileName.length - fileendtemplate.length);
+      console.log('Service Name', serviceName);
+      fs.readFile('TOSCA/' + fileName, 'utf8', function(err, data) {
         if (err) throw err;
         var yamlObject = yaml.safeLoad(data, 'utf8');
 
         var metadataObject = yamlObject.metadata;
         var inputObject = yamlObject.topology_template.inputs;
-        var nodeTemplateObject = yamlObject.topology_template.node_templates;
 
-        var outputServiceSpec =  {
-          "isBundle" : false,
-          "targetServiceSchema" : {
-            "@type" : "TOSCAService",
-            "@schemaLocation" : "@schemaLocation"
+        var outputServiceSpec = {
+          isBundle: false,
+          targetServiceSchema: {
+            '@type': 'TOSCAService',
+            '@schemaLocation': '@schemaLocation',
           },
-          "lifecycleStatus" : "CERTIFIED",
-          "@type" : "ServiceCatalog",
-          "resourceSpecification" : [ {
-            "name" : "name",
-            "id" : "id",
-            "href" : "href",
-            "version" : "version"
+          lifecycleStatus: 'CERTIFIED',
+          '@type': 'ServiceCatalog',
+          resourceSpecification: [ {
+            name: 'name',
+            id: 'id',
+            href: 'href',
+            version: 'version',
           }],
-          "description" : metadataObject.description,
-          "version" : metadataObject.version,
-          "@baseType" : "@baseType",
-          "lastUpdate" : "2000-01-23T04:56:07.000+00:00",
-          "name" : metadataObject.name,
-          "id" : metadataObject.UUID,
-          "href" : hostName+basePathName+'serviceSpecification/'+metadataObject.UUID,
-          "@schemaLocation" : "@schemaLocation",
-          "serviceSpecCharacteristic" : [],
-          "serviceSpecRelationship" : []
-          //"relatedParty" : [ {
+          description: metadataObject.description,
+          version: metadataObject.version,
+          '@baseType': '@baseType',
+          lastUpdate: '2000-01-23T04:56:07.000+00:00',
+          name: metadataObject.name,
+          id: metadataObject.UUID,
+          href: hostName + basePathName + 'serviceSpecification/' + metadataObject.UUID,
+          '@schemaLocation': '@schemaLocation',
+          serviceSpecCharacteristic: [],
+          serviceSpecRelationship: [],
+          // "relatedParty" : [ {
           //  "role" : "role",
           //  "validFor" : {
           //     "startDateTime" : "2000-01-23T04:56:07.000+00:00",
@@ -64,114 +63,114 @@ exports.getServiceSpecifications = function() {
           //  "name" : "name",
           //  "id" : "id",
           //  "href" : "href"
-          //}],
-          //"attachment" : [ {
+          // }],
+          // "attachment" : [ {
           //  "description" : "description",
           //  "href" : "href",
           //  "id" : "id",
           //  "type" : "type",
           //  "url" : "url"
-          //}],
-          //"validFor" : {
+          // }],
+          // "validFor" : {
           //  "startDateTime" : "2000-01-23T04:56:07.000+00:00",
           //  "endDateTime" : "2000-01-23T04:56:07.000+00:00"
-          //},
-        } ;
+          // },
+        };
         if (inputObject) {
-          var serviceSpecKeys=Object.keys(inputObject);
-          for (var i=0;i<serviceSpecKeys.length;i++) {
-            var characteristicObject=inputObject[serviceSpecKeys[i]];
-            var serviceSpecCharacteristic=
+          var serviceSpecKeys = Object.keys(inputObject);
+          for (var i = 0; i < serviceSpecKeys.length; i++) {
+            var characteristicObject = inputObject[serviceSpecKeys[i]];
+            var serviceSpecCharacteristic =
             {
-              "@type" : "TOSCAServiceCharacteristic",
-              "@valueSchemaLocation" : "@valueSchemaLocation",
-              "description" : characteristicObject.description,
-              "required" : characteristicObject.required,
-              "status" : characteristicObject.status,
-              "serviceSpecCharRelationship" : [],
-              "valueType" : characteristicObject.type,
-              "name" : serviceSpecKeys[i]
-              //"isUnique" : true,
-              //"maxCardinality" : 6,
-              //"minCardinality" : 0,
-              //"regex" : "regex",
-              //"validFor" : {
-                //"startDateTime" : "2000-01-23T04:56:07.000+00:00",
-                //"endDateTime" : "2000-01-23T04:56:07.000+00:00"
-              //},
-              //"@schemaLocation" : "@schemaLocation",
-              //"extensible" : true,
-              //"configurable" : true
-            }
-            var serviceSpecCharacteristicValueArray=[];
+              '@type': 'TOSCAServiceCharacteristic',
+              '@valueSchemaLocation': '@valueSchemaLocation',
+              description: characteristicObject.description,
+              required: characteristicObject.required,
+              status: characteristicObject.status,
+              serviceSpecCharRelationship: [],
+              valueType: characteristicObject.type,
+              name: serviceSpecKeys[i],
+              // "isUnique" : true,
+              // "maxCardinality" : 6,
+              // "minCardinality" : 0,
+              // "regex" : "regex",
+              // "validFor" : {
+              // "startDateTime" : "2000-01-23T04:56:07.000+00:00",
+              // "endDateTime" : "2000-01-23T04:56:07.000+00:00"
+              // },
+              // "@schemaLocation" : "@schemaLocation",
+              // "extensible" : true,
+              // "configurable" : true
+            };
+            var serviceSpecCharacteristicValueArray = [];
             if (characteristicObject.default){
-              var serviceSpecCharacteristicValue=
+              var serviceSpecCharacteristicValue =
                 {
-                  "isDefault" : true,
-                  "valueTo" : characteristicObject.default,
-                  //"rangeInterval" : "rangeInterval",
-                  //"regex" : "regex",
-                  //"unitOfMeasure" : "unitOfMeasure",
-                  //"validFor" : {
+                  isDefault: true,
+                  valueTo: characteristicObject.default,
+                  // "rangeInterval" : "rangeInterval",
+                  // "regex" : "regex",
+                  // "unitOfMeasure" : "unitOfMeasure",
+                  // "validFor" : {
                   //  "startDateTime" : "2000-01-23T04:56:07.000+00:00",
                   //  "endDateTime" : "2000-01-23T04:56:07.000+00:00"
-                  //},
-                  //"@type" : "@type",
-                  //"valueType" : "valueType",
-                  //"@schemaLocation" : "@schemaLocation"
-                  //"value" : "{}",
-                  //"valueFrom" : 1
-                }
-              serviceSpecCharacteristicValueArray.push(serviceSpecCharacteristicValue);      
+                  // },
+                  // "@type" : "@type",
+                  // "valueType" : "valueType",
+                  // "@schemaLocation" : "@schemaLocation"
+                  // "value" : "{}",
+                  // "valueFrom" : 1
+                };
+              serviceSpecCharacteristicValueArray.push(serviceSpecCharacteristicValue);
             }
 
-        
+
             serviceSpecCharacteristic.serviceSpecCharacteristicValue = serviceSpecCharacteristicValueArray;
 
             outputServiceSpec.serviceSpecCharacteristic.push(serviceSpecCharacteristic);
           }
         }
         outputServiceSpecArray.push(outputServiceSpec);
-        if (outputServiceSpecArray.length == fileArray.length) {  //if this is last file processed (async) then resolve promise
+        if (outputServiceSpecArray.length === fileArray.length) { // if this is last file processed (async) then resolve promise
           resolve(outputServiceSpecArray);
-        } 
-      });  
-    });    
+        }
+      });
+    });
 
   });
-}
+};
 
 
-/**Utility function to return subset of fields in serviceSpecificationArray */
+/** Utility function to return subset of fields in serviceSpecificationArray */
 exports.returnFields = function(inFields, inServiceSpecArray) {
-  var newOutput=[]
+  var newOutput = [];
   inServiceSpecArray.forEach(function(outputServiceSpec){
     var fieldsArray = inFields.split(',');
-    var newValue={};
+    var newValue = {};
     fieldsArray.forEach(function(field){
-      newValue[field]=outputServiceSpec[field];
+      newValue[field] = outputServiceSpec[field];
     });
     newOutput.push(newValue);
   });
   return newOutput;
 
-}
+};
 
-/**Utility function to filter serviceSpecificationArray based on a field */
+/** Utility function to filter serviceSpecificationArray based on a field */
 exports.filterArray = function(inField, inValue, inServiceSpecArray) {
-  var outputServiceSpecArray=[];
+  var outputServiceSpecArray = [];
   if (inValue) {
-    for (var i=0;i<inServiceSpecArray.length;i++){
-      if (inServiceSpecArray[i][inField]==inValue){
+    for (var i = 0; i < inServiceSpecArray.length; i++){
+      if (inServiceSpecArray[i][inField] === inValue){
         outputServiceSpecArray.push(inServiceSpecArray[i]);
       }
     }
 
   } else {
-    outputServiceSpecArray=inServiceSpecArray;
+    outputServiceSpecArray = inServiceSpecArray;
   }
   return outputServiceSpecArray;
-}
+};
 
 /**
  * List or find 'ServiceSpecification' objects
@@ -214,16 +213,16 @@ exports.filterArray = function(inField, inValue, inServiceSpecArray) {
  * targetServiceSchemaSchemaLocation String For filtering: This field provides a link to the schema describing the target service (optional)
  * returns List
  **/
-exports.listServiceSpecification = function(fields,name,type,schemaLocation,baseType,version,validForStartDateTime,validForEndDateTime,lastUpdate,lifecycleStatus,isBundle,resourceSpecificationVersion,resourceSpecificationName,resourceSpecificationType,attachmentName,attachmentType,attachmentUri,attachmentMimeType,serviceSpecCharacteristicName,serviceSpecCharacteristicValueType,serviceSpecCharacteristicConfigurable,serviceSpecCharacteristicType,serviceSpecCharacteristicSchemaLocation,serviceSpecCharacteristicValueSchemaLocation,serviceSpecCharacteristicMinCardinality,serviceSpecCharacteristicMaxCardinality,serviceSpecCharacteristicIsUnique,serviceSpecCharacteristicRegex,serviceSpecCharacteristicExtensible,relatedPartyRole,relatedPartyName,serviceSpecRelationshipType,serviceSpecRelationshipRole,serviceSpecRelationshipName,targetServiceSchemaType,targetServiceSchemaSchemaLocation) {
+exports.listServiceSpecification = function(fields, name, type, schemaLocation, baseType, version, validForStartDateTime, validForEndDateTime, lastUpdate, lifecycleStatus, isBundle, resourceSpecificationVersion, resourceSpecificationName, resourceSpecificationType, attachmentName, attachmentType, attachmentUri, attachmentMimeType, serviceSpecCharacteristicName, serviceSpecCharacteristicValueType, serviceSpecCharacteristicConfigurable, serviceSpecCharacteristicType, serviceSpecCharacteristicSchemaLocation, serviceSpecCharacteristicValueSchemaLocation, serviceSpecCharacteristicMinCardinality, serviceSpecCharacteristicMaxCardinality, serviceSpecCharacteristicIsUnique, serviceSpecCharacteristicRegex, serviceSpecCharacteristicExtensible, relatedPartyRole, relatedPartyName, serviceSpecRelationshipType, serviceSpecRelationshipRole, serviceSpecRelationshipName, targetServiceSchemaType, targetServiceSchemaSchemaLocation) {
   return new Promise(function(resolve, reject) {
 
     exports.getServiceSpecifications().then(function(serviceSpecArray){
 
-      var filteredServiceSpecArray=exports.filterArray('name',name,exports.filterArray('lifecycleStatus',lifecycleStatus,serviceSpecArray));
+      var filteredServiceSpecArray = exports.filterArray('name', name, exports.filterArray('lifecycleStatus', lifecycleStatus, serviceSpecArray));
 
       if (fields) { // only return the requested fields
 
-        var newServiceSpecArray=exports.returnFields(fields,filteredServiceSpecArray);
+        var newServiceSpecArray = exports.returnFields(fields, filteredServiceSpecArray);
         resolve(newServiceSpecArray);
 
       } else {
@@ -232,10 +231,8 @@ exports.listServiceSpecification = function(fields,name,type,schemaLocation,base
     });
 
   });
-  
-}
 
-
+};
 
 
 /**
@@ -249,11 +246,11 @@ exports.retrieveServiceSpecification = function(id) {
   return new Promise(function(resolve, reject) {
 
     exports.getServiceSpecifications().then(function(outputServiceSpecArray){
-      //find if array contains id
-      var resolved=false;
-      for (var i=0;i<outputServiceSpecArray.length;i++){
-        if (outputServiceSpecArray[i].id==id){
-          resolved=true;
+      // find if array contains id
+      var resolved = false;
+      for (var i = 0; i < outputServiceSpecArray.length; i++){
+        if (outputServiceSpecArray[i].id === id){
+          resolved = true;
           resolve([outputServiceSpecArray[i]]);
         }
       }
@@ -263,5 +260,5 @@ exports.retrieveServiceSpecification = function(id) {
     });
 
   });
-}
+};
 
