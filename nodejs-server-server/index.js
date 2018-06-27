4cb70dc13c99e14a8d811a7ba54560c376346158
Thomas <lester.thomas@vodafone.com>
2018-06-27 15:35:06 +0100

    Added selftest (node index.js selftest)

--- nodejs-server-server/index.js
+++ nodejs-server-server/index.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
-
+var newman = require('newman');
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
@@ -39,6 +39,22 @@ swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
   http.createServer(app).listen(serverPort, function() {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+
+    //if test flag then run tests and exit
+    console.log(process.argv);
+    if (process.argv[2]=="selftest") {
+      // call newman.run to pass `options` object and wait for callback
+      newman.run({
+        collection: require('./test/ServiceCatalogTest.postman_collection.json'),
+        environment: require('./test/ServiceCatalogTest.postman_environment.json'),
+        reporters: 'cli'
+      }, function (err, summary) {
+        if (err) { throw err; }
+        console.log("Selftest finished, exiting.");
+        process.exit();
+        });      
+    }
+
   });
 
 });
